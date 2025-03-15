54bc7d678bb2e50110d235e69fa0bf84d883b4ff
AZENKOUK AMINE <amine.azenkouk@gmail.com>
2025-03-15 16:13:18 +0000

    entring a year and a month and display the days of it

---
+++ Even_or_odd_number_using_function.py
@@ -0,0 +1,12 @@
+#this code is to show that if a number entered from the user is an odd number or even using a function ...
+
+def even_or_odd_number(num):
+    
+        if num% 2 ==0:
+            print(f'{num} is an Even number ')
+        
+        else :
+            print(f'{num} is an Odd number ')
+        
+
+even_or_odd_number(100)
---
+++ test.py
@@ -0,0 +1,89 @@
+
+
+# def priime(num):
+    
+#         if num% 2 ==0:
+#             print('Even')
+        
+#         else :
+#             print('Odd')
+        
+
+# priime(15)
+
+# def calc():
+#     sum=0
+#     def summ(x,y):
+#         return x+y 
+    
+#     for i in range(1,101):
+#         sum=summ(sum,i)
+#     return sum
+    
+# print(calc())
+
+# def summ(x,y):
+#         return x+y
+    
+# print(summ(1,2))
+
+# def swip(a,b):
+#     temp =a
+#     a=b
+#     b=temp
+#     return a,b
+
+# print(swip(1,2))
+
+# def fun(a,b,c,d,e):
+#     for i in range(a,b+1):
+#         if i % c ==d:
+#              print("even")
+#         elif i%c==e:
+#             print('even')
+#         else:
+#             print("3")
+            
+# print(fun(1,10,2,0,1))
+
+# def summ(x,y=100):
+#     return x+y
+
+# print(summ(y=5,x=5))
+
+
+#importannt    hhdekdhkjkqjhl
+
+# def add(*para):
+#     sum=0
+#     for i in para:
+#         sum+=i
+#     return sum
+
+# print(add(3,3,3,3,3,3,3))
+
+# def fac(n):
+#     if n == 1:
+#         return 1
+#     else :
+#         return n*fac(n-1)
+    
+# print(fac(4))
+
+def sum(n):
+    if n==0:
+        return 0 
+    else : 
+        return n+sum(n-1) 
+    
+print(sum(5))
+                                        #####
+# input a year and MONTHS AND PRINT THE DAYS OF THAT MONTH WITH MANY FUNCTIONS IN ONE CODE ######3##########SRP####
+                                        #####                
+
+    
+
+
+
+    
+    
